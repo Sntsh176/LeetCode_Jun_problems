561b763cafe8f63e5b0521de70cf8f086ff93acb
Santosh <santurock177@gmail.com>
2020-06-02 20:22:41 +0530

    adding with few changes

--- delete_node_linked_list.py
+++ delete_node_linked_list.py
@@ -40,3 +40,25 @@ class Solution:
         node.val = node.next.val
         node.next = node.next.next
         
+# ================================================
+# ================================================
+# ================================================
+
+
+# Definition for singly-linked list.
+# class ListNode:
+# def init(self, x):
+# self.val = x
+# self.next = None
+# class Solution:
+
+def deleteNode(self, node):
+    """
+    :type node: ListNode
+    :rtype: void Do not return anything, modify node in-place instead.
+    """
+    if(node.next != None):
+        temp = node.next
+        node.val = temp.val
+        node.next = temp.next
+        temp = None
